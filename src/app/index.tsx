4098ca7bab1e655d5809305fe5244c427b71be75
Abner Persio <binhopersio@gmail.com>
2025-01-21 07:24:22 -0300

    feat: initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/ui/components/button";
+
+export function App() {
+  return (
+    <div>
+      Hello JStack
+      <Button type="button">Clique aqui</Button>
+    </div>
+  );
+}
